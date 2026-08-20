0b0b69a2dbcf89ba277144915b037f7a5ad9b4e3
Anish D <anishd1922@gmail.com>
2026-08-20 18:34:07 +0530

    feat: add build route

---
+++ src/routes/build.tsx
@@ -0,0 +1,2 @@
+import { createFileRoute } from '@tanstack/react-router'
+export const Route = createFileRoute('/build')({ component: () => <section className="rrr-card"><h1>Build</h1><p>Build recommendations will use verified component compatibility.</p></section> })
