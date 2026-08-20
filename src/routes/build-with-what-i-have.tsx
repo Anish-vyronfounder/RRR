9896c093a8bac48ca5f0a56189ede78176733bb0
Anish D <anishd1922@gmail.com>
2026-08-20 18:34:10 +0530

    feat: add build inventory route

---
+++ src/routes/build-with-what-i-have.tsx
@@ -0,0 +1,2 @@
+import { createFileRoute } from '@tanstack/react-router'
+export const Route = createFileRoute('/build-with-what-i-have')({ component: () => <section className="rrr-card"><h1>Build With What I Have</h1><p>Add your components and RRR will check possible combinations.</p></section> })
